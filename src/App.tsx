be5b00b5ab59038fd43a10f525b896dd3b5340bb
mikoto2000 <mikoto2000@gmail.com>
2024-08-16 11:19:40 +0000

    パラメーター入力欄をコンポーネントに分離。

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
 
 import { AppBar, Box, Button, Divider, Link, Stack, TextField, Typography } from "@mui/material";
-import Grid from '@mui/material/Unstable_Grid2';
 import { DataGrid } from '@mui/x-data-grid';
 import { useState } from "react";
 import { Column, Parameter } from "./types";
 import { Service } from "./services/Service";
 import { TauriService } from "./services/TauriService";
+import { Parameters } from "./components/Parameters";
 
 function App() {
 
@@ -23,56 +23,6 @@ function App() {
   const [selectStatements, setSelectStatements] = useState<string[]>([]);
 
   const [error, setError] = useState<string>("");
-
-  const createParameterRow = (index: number, parameter: Parameter) => {
-
-    return (
-      <>
-        <Grid
-          key={index + "_name"}
-          xs={5}
-        >
-          <TextField
-            placeholder={parameter.name}
-            sx={{ width: "100%" }}
-            onChange={(e) => {
-              const newName = e.currentTarget.value;
-              const targetParameter = parameters[index];
-              targetParameter.name = newName;
-              setParameters([...parameters]);
-            }}
-          >
-          </TextField>
-        </Grid>
-        <Grid
-          key={index + "_value"}
-          xs={5}
-        >
-          <TextField
-            placeholder={parameter.value}
-            sx={{ width: "100%" }}
-          >
-          </TextField>
-        </Grid>
-        <Grid
-          key={index + "_button"}
-          xs={2}
-        >
-          <Button
-            variant="contained"
-            color="error"
-            sx={{ width: "100%", height: "100%" }}
-            onClick={() => {
-              const newParameters = parameters.filter((_, i) => i !== index);
-              setParameters([...newParameters]);
-            }}
-          >
-            削除
-          </Button>
-        </Grid>
-      </>
-    );
-  }
 
 
   return (
@@ -118,7 +68,7 @@ function App() {
                 const selectStatements = await service.find_select_statement(sql);
                 console.log(selectStatements);
                 setSelectStatements(selectStatements);
-              } catch(e) {
+              } catch (e) {
                 console.log(e);
                 setError(e as string);
               }
@@ -131,54 +81,53 @@ function App() {
       </Box >
       <p>{error}</p>
       <Divider sx={{ marginTop: "1em" }} />
-      <Typography>Parameters:</Typography>
-      <Grid container className="parameter-header">
-        <Grid xs={5}>name</Grid>
-        <Grid xs={5}>value</Grid>
-        <Grid xs={2} />
-      </Grid>
-      <Grid container className="parameter">
-        {parameters.map((e, i) => <Grid container key={i} xs={12}>{createParameterRow(i, e)}</Grid>)}
-      </Grid>
-      <Grid container className="parameter-control">
-        <Grid xs={10} />
-        <Grid xs={2}>
-          <Button
-            variant="contained"
-            sx={{ width: "100%", height: "4em" }}
-            onClick={() => setParameters([...parameters, { name: "", value: "" }])}
-          >
-            追加
-          </Button>
-        </Grid>
-      </Grid>
+      <Parameters
+        parameters={parameters}
+        setParameters={(newParameter: Parameter[]) => {
+          setParameters(newParameter);
+        }}
+      />
       <Divider sx={{ marginTop: "1em" }} />
+      <Typography>Select statements:</Typography>
       {
         showStatements ?
           <>
-            <Typography>Select statements:</Typography>
-            <Stack>
+            <Stack spacing={2}>
               {
                 selectStatements.map((e, i) => {
-                  return <Link key={i} onClick={() => { }}>{e}</Link>
+                  return <Link key={i} onClick={async () => {
+                    setError("");
+                    try {
+                      const [columns, rows] = await service.query(e);
+                      console.log(columns);
+                      console.log(rows);
+                      setShowResult(true);
+                      setColumns(columns);
+                      setQueryResult(rows);
+                    } catch (e) {
+                      console.log(e);
+                      setError(e as string);
+                    }
+                  }}>{e}</Link>
                 })}
             </Stack>
             <Divider />
           </>
           :
-          <></>
+          <>結果無し</>
       }
+      <Divider sx={{ marginTop: "1em" }} />
+      <Typography>Result:</Typography>
       {
         showResult ?
           <>
-            <Typography>Result:</Typography>
             <DataGrid
               columns={columns.sort((a, b) => a.ordinal - b.ordinal).map((c) => { return { field: c.name } })}
               rows={queryResult}
             />
           </>
           :
-          <></>
+          <>結果無し</>
       }
     </>
   );
---
+++ src/components/Parameters.tsx
@@ -0,0 +1,95 @@
+import { Button, FormControlLabel, Grid, Radio, RadioGroup, TextField, Typography } from "@mui/material";
+import { Parameter } from "../types";
+
+type ParametersProps = {
+  parameters: Parameter[],
+  setParameters: (newParameter: Parameter[]) => void,
+};
+
+export const Parameters: React.FC<ParametersProps> = ({ parameters, setParameters }) => {
+
+  const createParameterRow = (index: number, parameter: Parameter) => {
+
+    return (
+      <>
+        <Grid
+          key={index + "_name"}
+          xs={5}
+        >
+          <TextField
+            placeholder={parameter.name}
+            sx={{ width: "100%" }}
+            onChange={(e) => {
+              const newName = e.currentTarget.value;
+              const targetParameter = parameters[index];
+              targetParameter.name = newName;
+              setParameters([...parameters]);
+            }}
+          >
+          </TextField>
+        </Grid>
+        <Grid
+          key={index + "_value"}
+          xs={5}
+        >
+          <TextField
+            placeholder={parameter.value}
+            sx={{ width: "100%" }}
+          >
+          </TextField>
+        </Grid>
+        <Grid
+          key={index + "_button"}
+          xs={2}
+        >
+          <Button
+            variant="contained"
+            color="error"
+            sx={{ width: "100%", height: "100%" }}
+            onClick={() => {
+              const newParameters = parameters.filter((_, i) => i !== index);
+              setParameters([...newParameters]);
+            }}
+          >
+            削除
+          </Button>
+        </Grid>
+      </>
+    );
+  }
+
+
+  return (
+    <>
+      <Typography>Parameters:</Typography>
+      <RadioGroup
+        row
+        defaultValue="mybatis"
+      >
+        <FormControlLabel value="mybatis" control={<Radio />} label="MyBatis(#{name})" />
+        <FormControlLabel value="jpa" control={<Radio />} label="JPA(:name)" />
+        <FormControlLabel value="dapper" control={<Radio />} label="Dapper(@name)" />
+      </RadioGroup>
+      <Grid container className="parameter-header">
+        <Grid xs={5}>name</Grid>
+        <Grid xs={5}>value</Grid>
+        <Grid xs={2} />
+      </Grid>
+      <Grid container className="parameter">
+        {parameters.map((e, i) => <Grid container key={i} xs={12}>{createParameterRow(i, e)}</Grid>)}
+      </Grid>
+      <Grid container className="parameter-control">
+        <Grid xs={10} />
+        <Grid xs={2}>
+          <Button
+            variant="contained"
+            sx={{ width: "100%", height: "4em" }}
+            onClick={() => setParameters([...parameters, { name: "", value: "" }])}
+          >
+            追加
+          </Button>
+        </Grid>
+      </Grid>
+    </>
+  )
+}
