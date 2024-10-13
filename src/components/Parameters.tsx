a31beb8b5517bafe5ad4fc2e92cee556d499dd0c
mikoto2000 <mikoto2000@gmail.com>
2024-10-13 02:36:00 +0000

    $N 形式のパラメーターに対応。

--- src/components/Parameters.tsx
+++ src/components/Parameters.tsx
@@ -89,6 +89,7 @@ export const Parameters: React.FC<ParametersProps> = ({
         {/*<FormControlLabel value="mybatis" control={<Radio disabled />} label="MyBatis(#{name})" />*/}
         <FormControlLabel value="jpa" control={<Radio />} label="JPA(:name)" />
         <FormControlLabel value="dapper" control={<Radio />} label="Dapper(@name)" />
+        <FormControlLabel value="log" control={<Radio />} label="log($N)" />
       </RadioGroup>
       <Grid container className="parameter-header">
         <Grid xs={5}>name</Grid>
--- src/utils.ts
+++ src/utils.ts
@@ -6,25 +6,33 @@ export function replaceParameters(
   parameters: Parameter[]
 ) {
   let replacedQuery = query;
-  parameters.forEach((param: Parameter) => {
-    let replaceStr;
-    switch (parameterPattern) {
-      case "mybatis":
-        replaceStr = "#{" + param.name + "}";
-        break;
-      case "jpa":
-        replaceStr = ":" + param.name;
-        break;
-      case "dapper":
-        replaceStr = "@" + param.name;
-        break;
-      default: // do nothing
-    }
+  if (parameterPattern === "log") {
+    parameters.forEach((param: Parameter) => {
+      let replaceStr = "$" + param.name;
+      replacedQuery = replacedQuery.replace(replaceStr, param.value)
+    });
+    return replacedQuery;
+  } else {
+    parameters.forEach((param: Parameter) => {
+      let replaceStr;
+      switch (parameterPattern) {
+        case "mybatis":
+          replaceStr = "#{" + param.name + "}";
+          break;
+        case "jpa":
+          replaceStr = ":" + param.name;
+          break;
+        case "dapper":
+          replaceStr = "@" + param.name;
+          break;
+        default: // do nothing
+      }
 
-    if (replaceStr) {
-      replacedQuery = replacedQuery.replaceAll(replaceStr, param.value)
-    }
-  });
-  return replacedQuery;
+      if (replaceStr) {
+        replacedQuery = replacedQuery.replaceAll(replaceStr, param.value)
+      }
+    });
+    return replacedQuery;
+  }
 }
 
